906764aa6a946676b59e05c4fac4072ce95cb9fc
Yunchi Luo <mightyguava@gmail.com>
2026-08-23 21:08:37 -0400

    add mise, remove nvm

---
+++ fish/conf.d/mise.fish
@@ -0,0 +1,4 @@
+# mise version manager
+if command --query mise
+    mise activate fish | source
+end
--- fish/config.fish
+++ fish/config.fish
@@ -2,15 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# mise version manager
-if command --query mise
-    mise activate fish | source
-end
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
 # Device-local overrides (mac vs linux, work vs personal)
 if test -f ~/.config/fish/config.local.fish
     source ~/.config/fish/config.local.fish
